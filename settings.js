b92b6e7075d85413b3a620b2865db48478015615
Amasha-Zone-Bots <93105173+Amasha-Zone-Bots@users.noreply.github.com>
2023-01-31 12:23:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,18 +15,18 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.alivepic = `https://i.ibb.co/K0RtK0Z/IMG-20221021-WA0089.jpg `
-global.owner = ['94774071805'] //ur owner number
-global.ownername = "Kaveesha Sithum" //ur owner name
-global.ytname = "YT: kavee" //ur yt chanel name
+global.alivepic = `https://i.ibb.co/WzSFtDJ/Picsart-22-08-30-19-08-44-087.jpg `
+global.owner = ['94701651987'] //ur owner number
+global.ownername = "AMASHA INDUNIL" //ur owner name
+global.ytname = "YT: Amasha" //ur yt chanel name
 global.socialm = "Dark Nero" //ur github or insta name
 global.location = "Sri Lanka, Gampaha, Kurunegala" //ur location
 
 //bot bomdy 
-global.ownernomer = "94774071805" //ur number
-global.premium = ['94774071805'] //ur premium number
+global.ownernomer = "94701651987" //ur number
+global.premium = ['94701651987'] //ur premium number
 global.botname = 'DarkNero Bot MD V7' //ur bot name
-global.linkz = "https://chat.whatsapp.com/GkAmtuJ4jFE6axiNjoSiCX" //your theme url which will be displayed on whatsapp
+global.linkz = "https://chat.whatsapp.com/KwhBPTk1yvWLrpmGcgdA2h" //your theme url which will be displayed on whatsapp
 global.websitex = "https://www.youtube.com/@kaveebroyt5303" //ur website to be displayed
 global.botscript = 'https://github.com/Kaveeshasithum/DARKNERO--V7' //script link
 global.themeemoji = "🐨" //ur theme emoji
